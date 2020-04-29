44fc3f8dcd0b7abc49ffa34b64636837e05b1a81
zyang888 <zyang888@uw.edu>
2020-04-29 12:14:19 -0700

    added more styling

--- blog-homepage/src/index.js
+++ blog-homepage/src/index.js
@@ -12,17 +12,17 @@ class Square extends React.Component {
     return (
       <div className="square">
         <img src={this.props.value.image} alt={this.props.value.title}/>
-        <div>
-          <h1>{this.props.value.title}</h1>
-          <h2>{this.props.value.description}</h2>
-          <h3>{this.props.value.name}</h3>
-          <p className="date-duration">{this.props.value.date}</p>
-          <div>
-          <img className={(this.props.value.author.isMediumMember)? "green" : ""} src={this.props.value.author.image} alt={this.props.value.author.name}/>
-          <p>{this.props.value.author.name}<br/>
-          {monthNames[temp.getMonth()]} {temp.getDate()} &bull; {this.props.value.minutesToRead} min read</p>
+        <article>
+          <div className="article">
+            <h2>{this.props.value.title}</h2>
+            <h3>{this.props.value.description}</h3>
           </div>
-        </div>
+          <div className="info">
+            <img className={(this.props.value.author.isMediumMember)? "green" : ""} src={this.props.value.author.image} alt={this.props.value.author.name}/>
+            <p><strong>{this.props.value.author.name}</strong><br/>
+            {monthNames[temp.getMonth()]} {temp.getDate()} &bull; {this.props.value.minutesToRead} min read</p>
+          </div>
+        </article>
       </div>
     );
   }
@@ -31,21 +31,22 @@ class Square extends React.Component {
 class ForYou extends React.Component {
   render() {
     return (
-      <Container>
+      <section>
+      <div className="title"><h1>For you</h1></div>
         {renderSquare(yourdata)}
-      </Container>
+      </section>
     );
   }
 }
 
 
-
 class InCase extends React.Component {
   render() {
     return (
-      <Container>
+      <section>
+      <div className="title"><h1>In case you missed it</h1></div>
         {renderSquare(misseddata)}
-      </Container>
+      </section>
     );
   }
 }
